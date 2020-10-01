2817f6898fa924afad87aeadc3d4e88f2e5a1126
Erich Flock <erich.flock@check24.de>
2020-10-01 16:44:48 +0200

    create project structure with FeedItem class

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Erich Flock on 01.10.20.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

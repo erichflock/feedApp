82b06b6753e73253f945b62d4f997947370f287f
Erich Flock <erich.flock@check24.de>
2020-10-01 18:34:21 +0200

    Fix test name

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -19,7 +19,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         
         //Arrange
         let url = URL(string: "https://a-given-url.com")!

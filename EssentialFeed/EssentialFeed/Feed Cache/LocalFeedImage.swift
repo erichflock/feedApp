fba0af6d9e4589e8d33ac3e494797c9bf9109ec6
Erich Flock <erich.flock@check24.de>
2020-10-16 17:59:37 +0200

    Remove dependency from FeedImage

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Erich Flock on 16.10.20.
+//
+
+import Foundation
+
+public struct LocalFeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+
+    public init(id: UUID, description: String?, location: String?, url: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = url
+    }
+    
+}

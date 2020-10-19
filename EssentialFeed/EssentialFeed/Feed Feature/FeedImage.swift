294caf8425902d62cb5955374e4c0712df10b15a
Erich Flock <erich.flock@check24.de>
2020-10-19 17:11:15 +0200

    Remove testable attribute

--- EssentialFeed/EssentialFeed/Feed Feature/FeedImage.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImage.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct FeedImage: Equatable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let url: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
     
     public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
